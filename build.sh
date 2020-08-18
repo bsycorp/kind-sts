4d032e7404fb2bbcb05e0d3295e3e9812621f388
Nick Houghton <nhoughto@gmail.com>
2020-08-18 14:59:57 +1000

    fix extra args path

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 git clone https://github.com/bsycorp/kind
+export MINIKUBE_EXTRA_ARGS="$(cat extra-args)"
 cd kind
-export MINIKUBE_EXTRA_ARGS=$(cat extra-args)
 ./build.sh bsycorp/kind-sts:$DOCKER_IMAGE_TAG bsycorp/kind-sts:$KUBERNETES_VERSION
